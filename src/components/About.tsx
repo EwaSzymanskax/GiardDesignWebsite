110ecf45ad9fbf21a4c239455268eeb4264c6f05
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-10-03 13:19:08 +0200

    slide add

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ function About(){
             <div className="text-white pb-5 mx-5 lg:ml-8 xl:ml-24 lg:flex lg:flex-col">
                 <h1 className="text-xl pt-10 lg:text-xs lg:pt-40">O firmie</h1>
                 <h2 className=" font-montserrat font-medium text-3xl mt-5 lg:mx-0 lg:text-5xl lg:mt-4 xl:w-[200px]">Tworzymy z <span className="font-inter font-medium italic">pasją</span></h2>
-                <p className="text-sm mt-5 lg:text-base lg:mt-10 font-inter font-normal xl:w-[500px]">Każdy projekt to nowe wyzwanie. Dlatego nasz zespół tworzą wykwalifikowani projektanci oraz architekci, których zadaniem jest rozpoznanie i realizacja potrzeb każdego Klienta. Nasza specjalizacja to przestrzenie nowoczesne, które charakteryzuje minimalizm, geometria i elegancka prostota. Tworzymy ogrody małoobsługowe, dostosowane do współczesnego trybu życia.</p>
+                <p className="text-sm mt-5 lg:text-base lg:mt-10 font-inter font-normal xl:w-[500px]">Każdy projekt to nowe wyzwanie. Dlatego nasz zespół tworzą wykwalifikowani projektanci oraz architekci, których zadaniem jest rozpoznanie i realizacja potrzeb każdego Klienta. Nasza specjalizacja to przestrzenie nowoczesne, które charakteryzuje minimalizm, geometria i elegancka prostota. Tworzymy ogrody małoobsługowe, dostosowane do współczesnego trybu życia.</p> 
                 <button type="button" className="flex flex-row mt-16 mb-6 w-48 h-12 border border-white rounded-full text-base place-content-center pt-3 lg:w-[190px] lg:h-[50px] lg:mt-[72px] font-inter font-light">Poznaj nas bliżej<img className="h-4 pl-1 mt-1 ml-1" src={vectorWhite} alt="strzałka" /></button>
             </div>
         </div>
---
+++ src/components/Slide.tsx
@@ -0,0 +1,35 @@
+import vectorDown from "../Icons/VectorDown.png";
+
+interface Props{
+    title: string;
+    description: string;
+    image: string;
+}
+
+function Slide({title, description, image}: Props) {
+    return(
+        <div className="lg:min-h-[737px] lg:h-[calc(100vh-64px)]">
+                <div className="h-full">
+                    <div className="flex flex-col relative items-center lg:flex-row  top-5 lg:top-0 xl:pt-0 h-full">
+                        <div className=" h-min text-center lg:text-start lg:mr-8 xl:mr-16">
+                            <div className="flex flex-col place-content-center">
+                                <h1 className="flex font-montserrat font-medium text-2xl pt-5 mb-10 lg:text-6xl lg:ml-20 mx-auto lg:mb-11 xl:w-[560px] lg:leading-[70px]">{title}</h1>
+                                <p className="text-sm mt-0 mx-5 lg:text-base lg:ml-20 lg:mb-2.5 lg:mt-3 lg:w-[490px]">{description}</p>
+                            </div>
+                            <div className="flex flex-col mx-auto place-items-center lg:place-items-start lg:flex-row ">
+                                <button type="button" className="m-6 w-52 h-12 bg-green rounded-full text-base px-3 pt-1.5 pb-2 text-light-beige lg:mt-16 lg:ml-20">Skontaktuj się z nami</button>
+                                <button type="button" className="flex flex-row mb-5  w-64 h-12 border border-green rounded-full text-base place-content-center text-green pt-3 pb-3.5 lg:mt-16 lg:ml-4">Zobacz nasze realizacje<img className="w-4 h-4 text-green pl-1 mt-1"src={vectorDown} alt="vector" />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="flex flex-col mx-auto place-items-center lg:place-items-end z-1 lg:h-full relative">
+                            <img id="imgMove" className="w-full bg-beige place-items-center md:w-screen lg:mb-0 lg:min-w-[600px] xl:ml-[35px] xl:min-w-full
+                            z-1 duration-500 ease-out transition-all lg:h-full overflow-hidden relative" src={image} alt="ogród" />
+                        </div>   
+                    </div>
+                </div>
+            </div> 
+    )
+}
+
+export default Slide;
